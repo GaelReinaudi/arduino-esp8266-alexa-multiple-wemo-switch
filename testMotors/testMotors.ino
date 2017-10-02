96a9243d736a7e1562560b25f747f13c433d8cea
Gael Reinaudi <gael.reinaudi@gmail.com>
2017-10-02 15:46:45 -0400

    motor with L298N

--- testMotors/testMotors.ino
+++ testMotors/testMotors.ino
@@ -7,13 +7,13 @@
 // $Id: Random.pde,v 1.1 2011/01/05 01:51:01 mikem Exp mikem $
 
 #define IN1  16
-#define IN2  4
-#define IN3  5
+#define IN2  5
+#define IN3  4
 #define IN4  0
 
 #include "AccelStepper.h"
 
-AccelStepper stepper(AccelStepper::FULL4WIRE, IN1,IN2,IN3,IN4);
+AccelStepper stepper(AccelStepper::HALF4WIRE, IN1,IN2,IN3,IN4);
 
 void setup()
 {  
@@ -26,9 +26,10 @@ void loop()
       	// Random change to speed, position and acceleration
       	// Make sure we dont get 0 speed or accelerations
       	delay(1000);
-      	stepper.moveTo(rand() % 2000);
-      	stepper.setMaxSpeed((rand() % 500) + 1);
-      	stepper.setAcceleration((rand() % 2000) + 1);
+        int speed = (rand() % 100) + 1000;
+      	stepper.moveTo(rand() % 4000);
+      	stepper.setMaxSpeed(speed);
+      	stepper.setAcceleration((rand() % 20) + 1000);
     }
     stepper.run();
 }
