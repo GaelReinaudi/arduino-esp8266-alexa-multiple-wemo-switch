c2a0d6f68b0169c91b7b551b71820864d60d8e14
gael <gael.reinaudi@roardata.com>
2019-06-24 09:56:51 -0400

    trying out this: https://github.com/Aircoookie/Espalexa
    esp 32 core:
    https://github.com/espressif/arduino-esp32

--- wemos/wemos.ino
+++ wemos/wemos.ino
@@ -1,91 +1,99 @@
+/*
+ * This is an example on how to use Espalexa alongside an ESP8266WebServer.
+ */ 
+#include <Espalexa.h>
+ #ifdef ARDUINO_ARCH_ESP32
+#include <WiFi.h>
+#include <WebServer.h> //if you get an error here please update to ESP32 arduino core 1.0.0
+#else
 #include <ESP8266WiFi.h>
 #include <ESP8266WebServer.h>
-#include <WiFiUdp.h>
-#include <functional>
-#include "Switch.h"
-#include "UpnpBroadcastResponder.h"
-#include "CallbackFunction.h"
+#endif
 #include "SSID_PASSWORD.h"
 #include "rollerBlinds.h"
 
 // prototypes
 boolean connectWifi();
 
-//on/off callbacks 
-void officeLightsOn();
-void officeLightsOff();
-void kitchenLightsOn();
-void kitchenLightsOff();
+//callback functions
+void firstLightChanged(uint8_t brightness);
 
-// Change this before you flash
+// Change this!!
 const char* ssid = SSID;
 const char* password = PASSWORD;
 
 boolean wifiConnected = false;
-
-UpnpBroadcastResponder upnpBroadcastResponder;
-
-Switch *office = NULL;
-Switch *kitchen = NULL;
 RollerBlinds *rollerBlinds1 = 0;
+
+Espalexa espalexa;
+#ifdef ARDUINO_ARCH_ESP32
+WebServer server(80);
+#else
+ESP8266WebServer server(80);
+#endif
 
 void setup()
 {
   Serial.begin(115200);
-  delay(100);
-   
   // Initialise wifi connection
   wifiConnected = connectWifi();
   
-  if(wifiConnected) {
-    upnpBroadcastResponder.beginUdpMulticast();
-    
-    // Define your switches here. Max 14
-    // Format: Alexa invocation name, local port no, on callback, off callback
-    office = new Switch("bedroom blinds", 80, officeLightsOn, officeLightsOff);
-//    office = new Switch("livingroom blinds", 80, officeLightsOn, officeLightsOff);
-//    office = new Switch("les stores de la chambre", 80, officeLightsOn, officeLightsOff);
-//    office = new Switch("les stores du salon", 80, officeLightsOn, officeLightsOff);
+  if(wifiConnected){
+    server.on("/", HTTP_GET, [](){
+    server.send(200, "text/plain", "This is an example index page your server may send.");
+    });
+    server.on("/test", HTTP_GET, [](){
+    server.send(200, "text/plain", "This is a second subpage you may have.");
+    });
+    server.onNotFound([](){
+      if (!espalexa.handleAlexaApiCall(server.uri(),server.arg(0))) //if you don't know the URI, ask espalexa whether it is an Alexa control request
+      {
+        //whatever you want to do with 404s
+        server.send(404, "text/plain", "Not found");
+      }
+    });
 
-    Serial.println("Adding switches upnp broadcast responder");
-    upnpBroadcastResponder.addDevice(*office);
-//    upnpBroadcastResponder.addDevice(*kitchen);
-  }
-  else {
-    Serial.println("Cannot connect to wifi. Restarting...");
-    delay(100);
-    ESP.restart();
+    // Define your devices here.
+    espalexa.addDevice("livingroom blinds", firstLightChanged); //simplest definition, default state off
+
+    espalexa.begin(&server); //give espalexa a pointer to your server object so it can use your server instead of creating its own
+    //server.begin(); //omit this since it will be done by espalexa.begin(&server)
+  } else
+  {
+    while (1)
+    {
+      Serial.println("Cannot connect to WiFi. Please check data and reset the ESP.");
+      delay(2500);
+    }
   }
   rollerBlinds1 = new RollerBlinds(16, 5, 4, 0);
 }
  
 void loop()
 {
-	 if(wifiConnected){
-      upnpBroadcastResponder.serverLoop();
-      
-//      kitchen->serverLoop();
-      office->serverLoop();
-	 }
+   //server.handleClient() //you can omit this line from your code since it will be called in espalexa.loop()
+   espalexa.loop();
    rollerBlinds1->loop();
 }
 
-void officeLightsOn() {
-    Serial.print("Switch 1 turn on ...");
-    rollerBlinds1->goDown();
-}
+//our callback functions
+void firstLightChanged(uint8_t brightness) {
+    Serial.print("Device 1 changed to ");
+    
+    //do what you need to do here
 
-void officeLightsOff() {
-    Serial.print("Switch 1 turn off ...");
-    rollerBlinds1->goUp();
-}
-
-void kitchenLightsOn() {
-    Serial.print("Switch 2 turn on ...");
-}
-
-void kitchenLightsOff() {
-  Serial.print("Switch 2 turn off ...");
+    //EXAMPLE
+    if (brightness == 255) {
+      Serial.println("ON");
+      rollerBlinds1->goDown();
+    }
+    else if (brightness == 0) {
+      Serial.println("OFF");
+      rollerBlinds1->goUp();
+    }
+    else {
+      Serial.print("DIM "); Serial.println(brightness);
+    }
 }
 
 // connect to wifi – returns true if successful or false if not
@@ -99,28 +107,25 @@ boolean connectWifi(){
   Serial.println("Connecting to WiFi");
 
   // Wait for connection
-  Serial.print("Connecting ...");
+  Serial.print("Connecting...");
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
-    if (i > 10){
-      state = false;
-      break;
+    if (i > 20){
+      state = false; break;
     }
     i++;
   }
-  
+  Serial.println("");
   if (state){
-    Serial.println("");
     Serial.print("Connected to ");
     Serial.println(ssid);
     Serial.print("IP address: ");
     Serial.println(WiFi.localIP());
   }
   else {
-    Serial.println("");
     Serial.println("Connection failed.");
   }
-  
+  delay(100);
   return state;
 }
